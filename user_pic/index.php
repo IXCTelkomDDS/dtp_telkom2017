df08fe79f1360fa81958a23a7fc94c8c729179a6
lutfiambarwati <lutfiambarwati93@gmail.com>
2018-02-01 15:44:15 +0700

    update file user_pic/index.php

--- user_pic/index.php
+++ user_pic/index.php
@@ -1,10 +1,9 @@
 <?php error_reporting(0) // tambahkan untuk menghilangkan notice ?>
 
 <?php
-  //session_start();
 
   include "../header.php";
-  //include "../check_session.php";
+  include "../check_session.php";
 
 ?>
 
