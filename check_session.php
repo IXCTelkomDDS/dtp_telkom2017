20b833294b7c94a7e0e1861d194dd96ccb8d7d84
lutfiambarwati <lutfiambarwati93@gmail.com>
2018-02-01 14:34:51 +0700

    update file check_session.php

--- check_session.php
+++ check_session.php
@@ -3,7 +3,7 @@
 
      if(! isset($_SESSION['islogin'])){
 	    echo 'Forbidden access !<br>';
-		echo '<a href="login.php">Please login first !</a>';
+		echo '<a href="../login.php">Please login first !</a>';
 		exit;
 	 }
 ?>
