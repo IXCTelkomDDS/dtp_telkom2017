a0dfe2ccbf2c07621ff7114277e105eb27764cd0
lutfiambarwati <lutfiambarwati93@gmail.com>
2018-02-05 09:31:24 +0700

    update act_upload.php 5-2-2018

--- act_upload.php
+++ act_upload.php
@@ -38,8 +38,7 @@ if(isset($_POST['Submit'])) {
             <?php } else { ?>
               <script>
                 alert('Add Failed');
-                //location.href='user_pic/halaman-upload.php';
-                location.href='act_upload.php';
+                location.href='user_pic/halaman-upload.php';
               </script>
             <?php } ?>
     
