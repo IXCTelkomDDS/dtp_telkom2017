0bcb6a8669d82d9667b72e34b194d19f4a951490
lutfiambarwati <lutfiambarwati93@gmail.com>
2018-02-05 08:47:00 +0700

    update act_signup.php 5-2-2018

---
+++ act_signup.php
@@ -0,0 +1,30 @@
+<?php
+
+     include "koneksi_db.php";
+
+    if(isset($_POST['Submit2'])) {
+      $username     = $_POST['username'];
+      $password     = $_POST['password'];
+      //$MD5          = md5[$password]; //merubah variabel $ubah ke MD5
+      $lab_pic      = $_POST['lab_pic'];
+      //$user_type    = $_POST['User P I C'];
+
+        //pemeriksaan input selesai, bila benar langsung jalankan perintah selanjutnya
+        $sql = "INSERT INTO user_pic VALUES ('', '$username', '$password', '$lab_pic', 'User P.I.C')";
+	      $query = mysqli_query($connect, $sql);
+  
+	      if($query) { ?>
+    	    <script>
+	          alert('Add Successful');
+	          location.href='login.php';
+	        </script>
+
+    	  <?php } else { ?>
+	        <script>
+	          alert('Add Failed');
+	          location.href='signup.php';
+	        </script>
+
+      <?php } ?>    
+
+  <?php } ?>
