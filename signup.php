653803c40869773259114ca380fcb1773737c607
lutfiambarwati <lutfiambarwati93@gmail.com>
2018-02-05 11:51:32 +0700

    update signup.php 5-2-2018 ver3

--- signup.php
+++ signup.php
@@ -4,33 +4,6 @@
 
      include "koneksi_db.php";
 
-     if(isset($_POST['SignUp'])) {
-      $username     = $_POST['username'];
-      $password     = $_POST['password'];
-      //$MD5          = md5[$password]; //merubah variabel $ubah ke MD5
-      $lab_pic      = $_POST['lab_pic'];
-      //$user_type    = $_POST['User P I C'];
-
-        //pemeriksaan input selesai, bila benar langsung jalankan perintah selanjutnya
-        $sql = "INSERT INTO user_pic VALUES ('', '$username', '$password', '$lab_pic', 'User P.I.C')";
-        $query = mysqli_query($connect, $sql);
-  
-        if($query) { ?>
-          <script>
-            alert('Add Successful');
-            location.href='login.php';
-          </script>
-
-        <?php } else { ?>
-          <script>
-            alert('Add Failed');
-            location.href='signup.php';
-          </script>
-
-      <?php } ?>    
-
-  <?php } ?>
-
 ?>
 
 <!DOCTYPE html>
@@ -79,7 +52,7 @@
           </span>
         </div>
 
-        <form class="login100-form validate-form" role="form" method="post" enctype="multipart/form-data" action="">
+        <form class="login100-form validate-form" role="form" method="post" enctype="multipart/form-data" action="../act_signup.php">
 
           <div class="wrap-input100 validate-input m-b-26">
             <span class="label-input100">Username</span>
