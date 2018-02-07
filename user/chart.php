c6cb67f309495a168c7fbbdb14517da168294a92
lutfiambarwati <lutfiambarwati93@gmail.com>
2018-02-07 16:50:16 +0700

    update folder user_pic 7-2-2018

--- user/chart.php
+++ user/chart.php
@@ -12,7 +12,7 @@
 <body>
     <div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
     <div class="container-7 w-container"><a href="../index.php" class="brand w-nav-brand"><img src="../images/logo-telkom.png" width="80.5"></a>
-      <nav role="navigation" class="nav-menu w-nav-menu">
+      <nav role="navigation" class="nav-menu w-nav-menu" style="font-weight: bold">
 <a id="home" href="../index.php" class="navlink w-nav-link">Home</a>
 <a href="halaman-view.php" id="about" class="navlink w-nav-link">Daftar Kajian</a>
 <a href="halaman-prototype.php" id="prototype" class="navlink w-nav-link">Prototype</a>
--- user/halaman-prototype.php
+++ user/halaman-prototype.php
@@ -35,7 +35,7 @@
 <body class="body-3">
   <div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
     <div class="container-7 w-container"><a href="#" class="w-nav-brand"><img src="../images/logo-telkom.png" width="80.5"></a>
-      <nav role="navigation" class="w-nav-menu">
+      <nav role="navigation" class="w-nav-menu" style="font-weight: bold">
 <a href="../index.php" id="home" class="navlink w-nav-link">Home</a>
 <a href="halaman-view.php" id="home" class="navlink w-nav-link">Daftar Kajian</a>
 <a href="#prototype" id="prototype" class="navlink w-nav-link">Prototype</a>
