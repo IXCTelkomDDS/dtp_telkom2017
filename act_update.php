8a7e3bbb9aec066b29f5cc4d7768578e28466ead
lutfiambarwati <lutfiambarwati93@gmail.com>
2018-02-27 10:01:36 +0700

    update act_update.php 27-2-2018

--- act_update.php
+++ act_update.php
@@ -18,6 +18,7 @@
       $tgl_upload     = date("Y-m-d");
       $judul        = $_POST['JUDUL_UPLOAD'];
       $jenis_file     = $_POST['JENIS_FILE_UPLOAD'];
+      $unggulan       = $_POST['UNGGULAN'];
       $jenis_lab      = $_POST['JENIS_LAB_UPLOAD'];
       $deskripsi      = $_POST['DESKRIPSI_UPLOAD'];
       //$nama_file      = $_POST['NAMA_FILE_UPLOAD'];
@@ -27,7 +28,7 @@
 
       // Cek apakah user ingin mengubah fileya atau tidak
   if(isset($_POST['ubah_file'])) { // Jika user menceklis checkbox yang ada di form ubah, lakukan :
-      // Ambil data foto yang dipilih dari form
+      // Ambil data file yang dipilih dari form
         $file_name    = $_FILES['NAMA_FILE_UPLOAD']['name'];
         $file_tmp     = $_FILES['NAMA_FILE_UPLOAD']['tmp_name'];
 
@@ -41,7 +42,7 @@
           $nama_file = $file_name;
             move_uploaded_file($file_tmp, 'uploads/'.$nama_file);
 
-            $sql = "UPDATE upload_dtp SET TGL_UPLOAD = '$tgl_upload', JUDUL_UPLOAD = '$judul', JENIS_FILE_UPLOAD = '$jenis_file', JENIS_LAB_UPLOAD = '$jenis_lab', DESKRIPSI_UPLOAD = '$deskripsi', NAMA_FILE_UPLOAD = '$filebaru' WHERE ID_UPLOAD = '$id_upload'";
+            $sql = "UPDATE upload_dtp SET TGL_UPLOAD = '$tgl_upload', JUDUL_UPLOAD = '$judul', JENIS_FILE_UPLOAD = '$jenis_file', UNGGULAN = '$unggulan', JENIS_LAB_UPLOAD = '$jenis_lab', DESKRIPSI_UPLOAD = '$deskripsi', NAMA_FILE_UPLOAD = '$filebaru' WHERE ID_UPLOAD = '$id_upload'";
             $query = mysqli_query($connect,$sql);     
 
             if($query) { ?>
@@ -58,7 +59,7 @@
     
     <?php } ?>
   <?php } else {
-    $sql = "UPDATE upload_dtp SET TGL_UPLOAD = '$tgl_upload', JUDUL_UPLOAD = '$judul', JENIS_FILE_UPLOAD = '$jenis_file', JENIS_LAB_UPLOAD = '$jenis_lab', DESKRIPSI_UPLOAD = '$deskripsi' WHERE ID_UPLOAD = '$id_upload'";
+    $sql = "UPDATE upload_dtp SET TGL_UPLOAD = '$tgl_upload', JUDUL_UPLOAD = '$judul', JENIS_FILE_UPLOAD = '$jenis_file', UNGGULAN = '$unggulan', JENIS_LAB_UPLOAD = '$jenis_lab', DESKRIPSI_UPLOAD = '$deskripsi' WHERE ID_UPLOAD = '$id_upload'";
     $query = mysqli_query($connect,$sql);     
 
             if($query) { ?>
@@ -84,12 +85,13 @@
     $tgl_upload     = date("Y-m-d");
     $judul        = $_POST['JUDUL_UPLOAD'];
     $jenis_file     = $_POST['JENIS_FILE_UPLOAD'];
+    $unggulan       = $_POST['UNGGULAN'];
     $jenis_lab      = $_POST['JENIS_LAB_UPLOAD'];
     $deskripsi      = $_POST['DESKRIPSI_UPLOAD'];
     //$nama_file      = $_POST['NAMA_FILE_UPLOAD'];
     $url          = $_POST['URL'];
 
-        $sql2 = "UPDATE upload_dtp SET TGL_UPLOAD = '$tgl_upload', JUDUL_UPLOAD = '$judul', JENIS_FILE_UPLOAD = '$jenis_file', JENIS_LAB_UPLOAD = '$jenis_lab', DESKRIPSI_UPLOAD = '$deskripsi', URL = '$url' WHERE ID_UPLOAD = '$id_upload'";
+        $sql2 = "UPDATE upload_dtp SET TGL_UPLOAD = '$tgl_upload', JUDUL_UPLOAD = '$judul', JENIS_FILE_UPLOAD = '$jenis_file', UNGGULAN = '$unggulan', JENIS_LAB_UPLOAD = '$jenis_lab', DESKRIPSI_UPLOAD = '$deskripsi', URL = '$url' WHERE ID_UPLOAD = '$id_upload'";
         $query2 = mysqli_query($connect,$sql2);
 
             if($query2) { ?>
